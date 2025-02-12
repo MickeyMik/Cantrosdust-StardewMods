930189f02cbc0ef0fbd6b765106a58856a0eb6b8
MickeyBoi <83459911+MickeyMik@users.noreply.github.com>
2025-02-12 22:54:14 +0000

    - Added remark to "CurrentDefaultTickInterval" regarding the use of Game1 Millisecond references - Added comment to TickProgress set explaining "Math.Floor" usage

--- TimeSpeed/Framework/TimeHelper.cs
+++ TimeSpeed/Framework/TimeHelper.cs
@@ -10,12 +10,16 @@ namespace TimeSpeed.Framework
         ** Accessors
         *********/
         /// <summary>The game's default tick interval in milliseconds for the current location.</summary>
+        /// <remarks>10 minutes in-game is normally 7000 ms, but skull cavern, for example, adds 2000 ms per 10 minutes;
+        ///             It would be possible to use Game1.realMilliSecondsPerGameTenMinutes, I am unsure why it isn't used;
+        ///             For the same reason, I wonder if the ExtraMillisecondsPerInGameMinute needs to be multiplied by ten?</remarks>
         public int CurrentDefaultTickInterval => 7000 + (Game1.currentLocation?.ExtraMillisecondsPerInGameMinute ?? 0);
 
         /// <summary>The percentage of the <see cref="CurrentDefaultTickInterval"/> that's elapsed since the last tick.</summary>
         public double TickProgress
         {
             get => (double)Game1.gameTimeInterval / this.CurrentDefaultTickInterval;
+            // the math.floor stops rounding errors causing time-skip, e.g. 6999.5 milliseconds rounding to 7000 and then progressing time.
             set => Game1.gameTimeInterval = (int)(Math.Floor(value * this.CurrentDefaultTickInterval));
         }
     }
